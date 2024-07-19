c885e2f6ebc6230499a3c1b5a6e8d856ff66fa8b
diogo_gallina <diogogallina20@gmail.com>
2024-07-19 11:31:16 -0300

    feat: create storage adapter

---
+++ src/infra/cloud/adapters/protocols/storageAdapterInterface.ts
@@ -0,0 +1,7 @@
+export interface IStorageAdapter {
+  uploadFile(bucketName: string, fileName: string, fileContent: Buffer | string): Promise<void>;
+  getAllPathsInBucket(bucketName: string): Promise<string[]>;
+  createPathInBucket(bucketName: string, pathName: string): Promise<void>;
+  getAllFilesInPath(bucketName: string, pathName: string): Promise<any[]>;
+  getOneFileInPath(bucketName: string, pathName: string, key: string): Promise<any>;
+}
