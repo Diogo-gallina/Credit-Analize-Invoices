e65f47a06b9908f5763e0c273aafc9b092aee37b
diogo_gallina <diogogallina20@gmail.com>
2024-07-19 09:39:10 -0300

    feat: update necessary envs

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 8080,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
 };
