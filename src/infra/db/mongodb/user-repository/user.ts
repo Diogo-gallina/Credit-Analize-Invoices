9f4e9f65816d695d629cdc65157f31d63f0aea7f
diogo_gallina <diogogallina20@gmail.com>
2024-07-19 11:20:00 -0300

    feat: implements user repository

---
+++ src/infra/db/mongodb/user-repository/user.ts
@@ -0,0 +1,13 @@
+import { AddUserRepository } from 'data/protocols/addUserRepository';
+import { UserModel } from 'domain/models/user';
+import { AddUserModel } from 'domain/usecases/addUser';
+import { MongoHelper } from '../helpers/mongoHelper';
+
+export class UserMongoRepository implements AddUserRepository {
+  async add(userData: AddUserModel): Promise<UserModel> {
+    const userCollection = await MongoHelper.getCollection('users');
+    const result = await userCollection.insertOne(userData);
+    const { insertedId } = result;
+    return MongoHelper.map(await userCollection.findOne({ _id: insertedId }));
+  }
+}
