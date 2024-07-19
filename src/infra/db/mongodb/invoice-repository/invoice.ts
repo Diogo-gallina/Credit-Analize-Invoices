f2563fbe59ac79ec5583c332926d6cb0e9135fe0
diogo_gallina <diogogallina20@gmail.com>
2024-07-19 11:19:49 -0300

    feat: implements invoice repository

---
+++ src/infra/db/mongodb/invoice-repository/invoice.ts
@@ -0,0 +1,13 @@
+import { AddInvoiceRepository } from 'data/protocols/addInvoiceRepository';
+import { InvoiceModel } from 'domain/models/invoice';
+import { AddInvoiceModel } from 'domain/usecases/addInvoice';
+import { MongoHelper } from '../helpers/mongoHelper';
+
+export class InvoiceMongoRepository implements AddInvoiceRepository {
+  async add(invoiceData: AddInvoiceModel): Promise<InvoiceModel> {
+    const invoiceCollection = await MongoHelper.getCollection('invoices');
+    const result = await invoiceCollection.insertOne(invoiceData);
+    const { insertedId } = result;
+    return MongoHelper.map(await invoiceCollection.findOne({ _id: insertedId }));
+  }
+}
