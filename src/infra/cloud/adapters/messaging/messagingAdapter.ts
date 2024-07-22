75ba93eac5bb6a940530c6b1ecefe204749dda9d
diogo_gallina <diogogallina20@gmail.com>
2024-07-22 15:37:43 -0300

    feat: implements messaging adapter sendInvoiceToQueue

--- src/infra/cloud/adapters/messaging/messagingAdapter.ts
+++ src/infra/cloud/adapters/messaging/messagingAdapter.ts
@@ -1,14 +1,17 @@
 import { IMessagingHelper } from 'infra/cloud/aws/protocols/messagingHelperInterface';
 import { IDataForAnalysis, IMessagingAdapter } from '../protocols/messagingAdapterInterface';
 
+const SEND_QUEUE_NAME = 'invoice-data-extracted.fifo';
+const MESSAGE_GROUP_ID = 'invoiceGroup';
+
 export class MessagingAdapter implements IMessagingAdapter {
   constructor(private readonly messagingHelper: IMessagingHelper) {
     this.messagingHelper = messagingHelper;
   }
   sendInvoiceToQueue(message: IDataForAnalysis): Promise<void> {
-    return this.messagingHelper.sendInvoiceToQueue(message);
+    return this.messagingHelper.sendMessageToQueue(message, SEND_QUEUE_NAME, MESSAGE_GROUP_ID);
   }
   consumesAnalysisResult(): Promise<string> {
-    return this.messagingHelper.consumesAnalysisResult();
+    return this.messagingHelper.consumesMessage();
   }
 }
