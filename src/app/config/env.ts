9e8f357aef35ac479117c5116cff1a08c5ace843
diogo_gallina <diogogallina20@gmail.com>
2024-07-23 16:31:21 -0300

    remove aws config to env.ts

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,6 +5,4 @@ dotenv.config();
 export default {
   mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 8080,
-  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
-  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
 };
