02e51b21f7f1e4dc3f0a7cbde796f7de1c6863a2
diogo_gallina <diogogallina20@gmail.com>
2024-07-29 17:21:44 -0300

    feat: add invoice file information on request

--- src/app/routes/invoiceRoutes.ts
+++ src/app/routes/invoiceRoutes.ts
@@ -1,7 +1,8 @@
 import { routeAdapter } from '@app/adapters/expressRoutesAdapter';
 import { makeInvoiceController } from '@app/factories/addInvoice';
+import { upload } from '@app/middlewares';
 import { Router } from 'express';
 
 export default (router: Router): void => {
-  router.post('/analyze-invoice', routeAdapter(makeInvoiceController()));
+  router.post('/analyze-invoice', upload.single('invoiceFile'), routeAdapter(makeInvoiceController()));
 };
