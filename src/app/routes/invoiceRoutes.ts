cf096a116445ec79a3e92e0698a808a85d316623
diogo_gallina <diogogallina20@gmail.com>
2024-07-31 17:22:27 -0300

    feat: create authentication route

--- src/app/routes/invoiceRoutes.ts
+++ src/app/routes/invoiceRoutes.ts
@@ -1,8 +1,9 @@
 import { routeAdapter } from '@app/adapters/expressRoutesAdapter';
 import { makeInvoiceController } from '@app/factories/addInvoice';
 import { upload } from '@app/middlewares';
+import { authenticateJWT } from '@app/middlewares/authenticate';
 import { Router } from 'express';
 
 export default (router: Router): void => {
-  router.post('/analyze-invoice', upload.single('invoiceFile'), routeAdapter(makeInvoiceController()));
+  router.post('/analyze-invoice', authenticateJWT, upload.single('invoiceFile'), routeAdapter(makeInvoiceController()));
 };
