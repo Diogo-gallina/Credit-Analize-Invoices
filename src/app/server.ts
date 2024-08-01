d938bdc465f5b4866c2874aa7022de199dcc4363
diogo_gallina <diogogallina20@gmail.com>
2024-08-01 16:52:32 -0300

    feat: create cron job schedule

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongoHelper';
+import { schedule } from 'node-cron';
 import env from './config/env';
+import { makeConsumesAnalyzedResultUseCase } from './factories/consumesAnalyzedResult';
 
 dotenv.config();
 
@@ -8,5 +10,10 @@ MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default;
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+
+    const cronJobLoopTimeExpression = '*/5 * * * * *';
+    schedule(cronJobLoopTimeExpression, async () => {
+      await makeConsumesAnalyzedResultUseCase().execute();
+    });
   })
   .catch(console.error);
