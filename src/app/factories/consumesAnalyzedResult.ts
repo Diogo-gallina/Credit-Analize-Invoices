4ba6d7a6506b8dbca3664d3aa4eb733abc5af684
diogo_gallina <diogogallina20@gmail.com>
2024-08-01 16:49:24 -0300

    feat: create makeConsumesAnalyzedResultUseCase factorie

---
+++ src/app/factories/consumesAnalyzedResult.ts
@@ -0,0 +1,12 @@
+import { ConsumesAnalyzedResultsUseCase } from '@app/use-cases/consumes-analyzed-results/consumesAnalyzedResults';
+import { DbAddAnalizedResult } from '@data/use-cases/add-analyze-result/dbAddAnalyzeResult';
+import { MessagingAdapter } from '@infra/cloud/adapters/messaging/messagingAdapter';
+import { sqsHelper } from '@infra/cloud/aws/helpers/messaging/sqsHelper';
+import { AnalyzedResultMongoRepository } from '@infra/db/mongodb/analyzed-result-repository/anlyzedResult';
+
+export const makeConsumesAnalyzedResultUseCase = (): ConsumesAnalyzedResultsUseCase => {
+  const messagingAdapter = new MessagingAdapter(sqsHelper);
+  const analyzedResultRepository = new AnalyzedResultMongoRepository();
+  const dbAddAnalyzedResult = new DbAddAnalizedResult(analyzedResultRepository);
+  return new ConsumesAnalyzedResultsUseCase(messagingAdapter, dbAddAnalyzedResult);
+};
