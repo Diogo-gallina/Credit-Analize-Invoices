6e4e62c0c6eaab54b9720a80b47ae51d82d15f88
diogo_gallina <diogogallina20@gmail.com>
2024-08-01 15:08:24 -0300

    feat: create invoice result route

--- src/app/routes/invoiceRoutes.ts
+++ src/app/routes/invoiceRoutes.ts
@@ -1,9 +1,11 @@
 import { routeAdapter } from '@app/adapters/expressRoutesAdapter';
 import { makeInvoiceController } from '@app/factories/addInvoice';
+import { makeInvoiceResultController } from '@app/factories/invoiceResult';
 import { upload } from '@app/middlewares';
 import { authenticateJWT } from '@app/middlewares/authenticate';
 import { Router } from 'express';
 
 export default (router: Router): void => {
   router.post('/analyze-invoice', authenticateJWT, upload.single('invoiceFile'), routeAdapter(makeInvoiceController()));
+  router.get('/analyze-result', authenticateJWT, routeAdapter(makeInvoiceResultController()));
 };
