9c4d701a232d7db3db8dfe521a5004151d6c83ae
diogo_gallina <diogogallina20@gmail.com>
2024-08-01 15:15:07 -0300

    refactor: change repository interface names

--- src/data/protocols/userRepository.ts
+++ src/data/protocols/userRepository.ts
@@ -1,7 +1,7 @@
 import { UserModel } from '@domain/models/user';
 import { AddUserModel } from '@domain/use-cases/addUser';
 
-export interface UserRepository {
+export interface IUserRepository {
   add(userData: AddUserModel): Promise<UserModel>;
   findOneByEmail(email: string): Promise<UserModel>;
 }
