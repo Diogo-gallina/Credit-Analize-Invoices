2f5d4727a77cb94300e821fdc397a7fa54fa7d22
diogo_gallina <diogogallina20@gmail.com>
2024-08-05 14:02:09 -0300

    refactor: change field error logic

--- src/presentation/controllers/invoice.controller.ts
+++ src/presentation/controllers/invoice.controller.ts
@@ -26,10 +26,12 @@ export class InvoiceController implements Controller {
     try {
       const invoiceRequestBody: UploadInvoiceDto = httpRequest.body;
       const invoiceFile = httpRequest.file;
-      const { name, email, document } = invoiceRequestBody!;
-      if (!name || !email || !document || !invoiceFile) {
-        return badRequest(new MissingParamError('Missing required fields'));
+      const requiredFields = ['name', 'email', 'document'];
+      for (const field of requiredFields) {
+        if (!invoiceRequestBody[field]) return badRequest(new MissingParamError(field));
+        if (!invoiceFile) return badRequest(new MissingParamError('file'));
       }
+      const { name, email, document } = invoiceRequestBody!;
       const user = await this.dbAddUser.add({ name, document, email, createdAt: new Date() });
       const fileName = `${Date.now()}_${invoiceFile.originalname}`;
       const url = await this.uploadInvoiceUseCase.execute(invoiceFile, email, fileName);
