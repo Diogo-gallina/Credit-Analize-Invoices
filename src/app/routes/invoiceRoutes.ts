fca57a95ef08a36d431c4ea24d83b3cfcb900da0
diogo_gallina <diogogallina20@gmail.com>
2024-08-05 13:48:03 -0300

    feat: add analyze history route

--- src/app/routes/invoiceRoutes.ts
+++ src/app/routes/invoiceRoutes.ts
@@ -1,5 +1,6 @@
 import { routeAdapter } from '@app/adapters/expressRoutesAdapter';
 import { makeInvoiceController } from '@app/factories/addInvoice';
+import { makeInvoiceHistoryController } from '@app/factories/invoiceHistory';
 import { makeInvoiceResultController } from '@app/factories/invoiceResult';
 import { upload } from '@app/middlewares';
 import { authenticateJWT } from '@app/middlewares/authenticate';
@@ -7,5 +8,6 @@ import { Router } from 'express';
 
 export default (router: Router): void => {
   router.post('/analyze-invoice', authenticateJWT, upload.single('invoiceFile'), routeAdapter(makeInvoiceController()));
+  router.get('/analyze-history', authenticateJWT, routeAdapter(makeInvoiceHistoryController()));
   router.get('/analyze-result', authenticateJWT, routeAdapter(makeInvoiceResultController()));
 };
