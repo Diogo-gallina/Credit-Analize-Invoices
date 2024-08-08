afe301f2e1bb4f9fd2271a272b81249468440ed0
diogo_gallina <diogogallina20@gmail.com>
2024-08-08 08:33:07 -0300

    feat: create find one user by id method

--- src/data/protocols/userRepository.ts
+++ src/data/protocols/userRepository.ts
@@ -4,4 +4,5 @@ import { AddUserModel } from '@domain/use-cases/addUser';
 export interface IUserRepository {
   add(userData: AddUserModel): Promise<UserModel>;
   findOneByEmail(email: string): Promise<UserModel>;
+  findOneById(userId: string): Promise<UserModel>;
 }
--- src/infra/db/mongodb/user-repository/user.ts
+++ src/infra/db/mongodb/user-repository/user.ts
@@ -1,15 +1,10 @@
 import { IUserRepository } from '@data/protocols/userRepository';
 import { UserModel } from '@domain/models/user';
 import { AddUserModel } from '@domain/use-cases/addUser';
+import { ObjectId } from 'mongodb';
 import { MongoHelper } from '../helpers/mongoHelper';
 
 export class UserMongoRepository implements IUserRepository {
-  async findOneByEmail(email: string): Promise<UserModel | null> {
-    const userCollection = await MongoHelper.getCollection('users');
-    if (!userCollection) throw new Error('Failed to get collection');
-    return MongoHelper.map(await userCollection.findOne({ email })) || null;
-  }
-
   async add(userData: AddUserModel): Promise<UserModel> {
     const userCollection = await MongoHelper.getCollection('users');
     if (!userCollection) throw new Error('Failed to get collection');
@@ -17,5 +12,17 @@ export class UserMongoRepository implements IUserRepository {
     if (!result || !result.insertedId) throw new Error('Failed to insert user');
     const { insertedId } = result;
     return MongoHelper.map(await userCollection.findOne({ _id: insertedId }));
+  }
+
+  async findOneByEmail(email: string): Promise<UserModel | null> {
+    const userCollection = await MongoHelper.getCollection('users');
+    if (!userCollection) throw new Error('Failed to get collection');
+    return MongoHelper.map(await userCollection.findOne({ email })) || null;
+  }
+
+  async findOneById(userId: string): Promise<UserModel | null> {
+    const userCollection = await MongoHelper.getCollection('users');
+    if (!userCollection) throw new Error('Failed to get collection');
+    return MongoHelper.map(await userCollection.findOne({ _id: new ObjectId(userId) })) || null;
   }
 }
