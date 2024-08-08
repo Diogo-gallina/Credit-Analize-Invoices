6037ef3ff5351a83afd00266f296742829482cca
diogo_gallina <diogogallina20@gmail.com>
2024-08-08 08:35:03 -0300

    feat: add new instances to make consumes analyzed result factory

--- src/app/factories/consumesAnalyzedResult.ts
+++ src/app/factories/consumesAnalyzedResult.ts
@@ -1,12 +1,25 @@
 import { ConsumesAnalyzedResultsUseCase } from '@app/use-cases/consumesAnalyzedResults/consumesAnalyzedResults';
 import { DbAddAnalizedResult } from '@data/use-cases/add-analyze-result/dbAddAnalyzeResult';
+import { EmailAdapter } from '@infra/cloud/adapters/email/emailAdapter';
 import { MessagingAdapter } from '@infra/cloud/adapters/messaging/messagingAdapter';
-import { sqsHelper } from '@infra/cloud/aws/helpers/messaging/sqsHelper';
+import { sqsHelper } from '@infra/cloud/lib/aws/helpers/messaging/sqsHelper';
+import { nodemailerHelper } from '@infra/cloud/lib/nodemailer/helper/email/nodemailerHelper';
 import { AnalyzedResultMongoRepository } from '@infra/db/mongodb/analyzed-result-repository/anlyzedResult';
+import { InvoiceMongoRepository } from '@infra/db/mongodb/invoice-repository/invoice';
+import { UserMongoRepository } from '@infra/db/mongodb/user-repository/user';
 
 export const makeConsumesAnalyzedResultUseCase = (): ConsumesAnalyzedResultsUseCase => {
   const messagingAdapter = new MessagingAdapter(sqsHelper);
   const analyzedResultRepository = new AnalyzedResultMongoRepository();
   const dbAddAnalyzedResult = new DbAddAnalizedResult(analyzedResultRepository);
-  return new ConsumesAnalyzedResultsUseCase(messagingAdapter, dbAddAnalyzedResult);
+  const userRepository = new UserMongoRepository();
+  const invoiceRepository = new InvoiceMongoRepository();
+  const emailAdapter = new EmailAdapter(nodemailerHelper);
+  return new ConsumesAnalyzedResultsUseCase(
+    messagingAdapter,
+    dbAddAnalyzedResult,
+    userRepository,
+    invoiceRepository,
+    emailAdapter,
+  );
 };
