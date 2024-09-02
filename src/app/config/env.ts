8a84fe6172388cf7f8cd20b90ca5dbdd36d9ec07
diogo_gallina <diogogallina20@gmail.com>
2024-09-02 11:05:09 -0300

    chore: add localhost default url for mongoDB

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  mongoUrl: process.env.MONGO_URL,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/credit-analyze-invoice',
   port: process.env.PORT || 8080,
 };
